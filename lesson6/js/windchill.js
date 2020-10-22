35775397246a21a2d93496b7bf4fee5a91157078
erilei <ken17018@byui.edu>
2020-10-22 11:05:33 -0600

    js id

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,9 +1,9 @@
 let t = parseFloat(document.querySelector(".temp").innerHTML)
 let s = parseFloat(document.querySelector(".wind-speed").innerHTML)
-let chillFactor = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s,0.16)) + (0.4275 * t * Math.pow(s,0.16))
 if ( t <= 50.0 && s > 3.0) {
-    document.querySelector(".wind-chill").innerHTML = Math.ceil(chillFactor)
+    let chillFactor = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s,0.16)) + (0.4275 * t * Math.pow(s,0.16))
+    document.querySelector("#chill").innerHTML = Math.ceil(chillFactor)
 }
 else {
-    document.querySelector(".colweight.chill").innerHTML = "N/A"
+    document.querySelector("#chill").innerHTML = "N/A"
 }
