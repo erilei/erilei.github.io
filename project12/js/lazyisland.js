84e4e2997f4850e27ef664a1a7945ff37198ebb1
erilei <ken17018@byui.edu>
2020-12-09 15:10:11 -0700

    dot device

--- project12/js/lazyisland.js
+++ project12/js/lazyisland.js
@@ -25,7 +25,7 @@ function carousel() {
 slideIndex++;
 if (slideIndex > x.length) {slideIndex = 1} 
 x[slideIndex-1].style.display = "block"; 
-setTimeout(showSlides, 4000); // Change image every 2 seconds
+
 }
 var slideIndex = 1;
 showSlides(slideIndex);
@@ -38,15 +38,11 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1} 
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none"; 
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
+
 }
